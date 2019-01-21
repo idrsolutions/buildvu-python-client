d05edab01255c6af4bcdf6bc6f28f00e8c08cad5
Daniel Warren <danielwarren@idrsolutions.com>
2019-01-21 15:17:51 +0000

    Update version number

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def get_readme():
 
 setup(
     name='buildvu',
-    version='3.2.1',
+    version='4.0.0',
     description="Python API for IDRSolutions' Buildvu Microservice Example",
     long_description=get_readme(),
     url='https://github.com/idrsolutions/buildvu-python-client',
